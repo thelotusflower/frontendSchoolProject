cfc6a2809e6d913085dc117febb4eab752715d67
thelotusflower <thelotusflower@DESKTOP-4DNJU9U.localdomain>
2020-01-15 21:28:19 +0500

    basic posts fetching functionality

---
+++ my-app/src/post-loader.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+async function getPosts(offset, publicId) {
+    const api_token = '4e007337ab62725f6719f9bba9f311fbc908ca66d4b0213b2f4d79526c1ff8e6e1c016e9d491d855c4ad5';
+    let count = 100;
+    let publicIdArg = publicId.id ? `ownerId=${publicId.id}` : `domain=${publicId.domain}`;
+    let url = `https://api.vk.com/method/wall.get?${publicIdArg}&v=5.52&access_token=${api_token}&count=${count}&offset=${offset}`;
+
+    let response = await fetch(url);
+    let body = await response.json()
+    
+    return body.response.items;
+}
+
+async function getPostsFromPublic(publicId) {
+    const dayAgo = new Date();
+    dayAgo.setDate(dayAgo.getDate() - 1);
+
+    let posts = [];
+    let offset = 0;
+    
+    posts = posts.concat(await getPosts(offset, publicId));    
+    posts = posts.filter((el) => { return new Date(el.date * 1000) > dayAgo});
+
+    return posts;
+
+}
+
+async function loadPostsFromGroups(groupIds) {
+    let posts = [];
+    
+    for(let group of groupIds) {
+        posts = posts.concat(await getPostsFromPublic(group));
+    }
+    console.log(`posts ${posts.length}`);
+    return posts;
+}
+
+
+console.log(loadPostsFromGroups([{'id': '-171004087', 'domain': null}, {'id': null, 'domain': 'mudachyo'}]).length);
